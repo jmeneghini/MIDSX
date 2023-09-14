89a7e097971767f335bba1f64d02648ac0fa323f
jmeneghini <meneghinijohn@gmail.com>
2023-09-14 13:05:18 -0400

    fixed z=1 case for direction change

--- src/MIDSX/Core/particle.cpp
+++ src/MIDSX/Core/particle.cpp
@@ -1,6 +1,17 @@
 #include "Core/particle.h"
 
+namespace {
+    const double EPSILON = 1E-9;
+}
+
 Eigen::Vector3d ParticleHelpers::rotateDirection(const Eigen::Vector3d &vector, const double &theta, const double &phi) {
+    // check if pointing in z direction
+    if (1 - abs(vector.z()) < EPSILON) {
+        double new_dir_x = vector.z() * sin(theta) * cos(phi);
+        double new_dir_y = vector.z() * sin(theta) * sin(phi);
+        double new_dir_z = vector.z() * cos(theta);
+        return {new_dir_x, new_dir_y, new_dir_z};
+    }
     double new_dir_x = vector.x() * cos(theta) - sin(theta)/(sqrt(1 - vector.z()*vector.z())) * (vector.x()*vector.z()*cos(phi) + vector.y()*sin(phi));
     double new_dir_y = vector.y() * cos(theta) - sin(theta)/(sqrt(1 - vector.z()*vector.z())) * (vector.y()*vector.z()*cos(phi) - vector.x()*sin(phi));
     double new_dir_z = vector.z() * cos(theta) + sqrt(1 - vector.z()*vector.z()) * sin(theta) * cos(phi);
