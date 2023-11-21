2a90f495de660670156593f085f0087943447351
jmeneghini <meneghinijohn@gmail.com>
2023-11-21 11:48:41 -0500

    redoing python wrapper

--- python/__init__.py
+++
@@ -1,3 +0,0 @@
-from .pymidsx import *
-from .interaction_data_wrapper import InteractionData # overwrite the InteractionData wrapper with the python version
-
--- python/_pymidsx/voxel_grid_wrapper.h
+++
@@ -1,5 +0,0 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/eigen.h>
-#include <MIDSX/Core.h>
-
-void wrapVoxelGrid(py::module& m);
