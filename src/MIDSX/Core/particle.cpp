7a7eb5cc93f57fc3625e30cbf3b736acf7353862
jmeneghini <meneghinijohn@gmail.com>
2024-01-16 19:43:08 -0500

    Revert "Revert "switched to quaternion rotations for scattering angles""
    This reverts commit 5f41d364608d3791827360feed2bf0a2fbda7cef.

--- src/MIDSX/Core/particle.cpp
+++ src/MIDSX/Core/particle.cpp
@@ -5,18 +5,35 @@ namespace {
 }
 
 Eigen::Vector3d ParticleHelpers::rotateDirection(const Eigen::Vector3d &vector, const double &theta, const double &phi) {
-    // check if pointing in z direction
-    if (1 - abs(vector.z()) < EPSILON) {
-        double new_dir_x = vector.z() * sin(theta) * cos(phi);
-        double new_dir_y = vector.z() * sin(theta) * sin(phi);
-        double new_dir_z = vector.z() * cos(theta);
-        return {new_dir_x, new_dir_y, new_dir_z};
+    // First find a perpendicular vector to the given vector.
+    // Must be normalized for use with quaternion rotation
+    Eigen::Vector3d perp_vector = getPerpendicularVector(vector).normalized();
+
+    // Using quaternion rotation, rotate the vector about the perpendicular vector by theta (polar angle)
+    // Eigen has some nice helper functions for this
+    auto rotation = Eigen::AngleAxisd(theta, perp_vector);
+    Eigen::Vector3d rotated_vector = rotation * perp_vector;
+
+    // Now rotate about the direction vector by phi (azimuthal angle)
+    rotation = Eigen::AngleAxisd(phi, vector);
+    rotated_vector = rotation * rotated_vector;
+
+    return rotated_vector;
+}
+
+Eigen::Vector3d ParticleHelpers::getPerpendicularVector(const Eigen::Vector3d &vector) {
+    // Find the smallest component
+    // Will be used to find the perp vector
+    if (std::abs(vector.x()) <= std::abs(vector.y()) && std::abs(vector.x()) <= std::abs(vector.z())) {
+        // x is the smallest component
+        return Eigen::Vector3d::UnitX().cross(vector);
+    } else if (std::abs(vector.y()) <= std::abs(vector.z())) {
+        // y is the smallest component
+        return Eigen::Vector3d::UnitY().cross(vector);
+    } else {
+        // z is the smallest component
+        return Eigen::Vector3d::UnitZ().cross(vector);
     }
-    double new_dir_x = vector.x() * cos(theta) - sin(theta)/(sqrt(1 - vector.z()*vector.z())) * (vector.x()*vector.z()*cos(phi) + vector.y()*sin(phi));
-    double new_dir_y = vector.y() * cos(theta) - sin(theta)/(sqrt(1 - vector.z()*vector.z())) * (vector.y()*vector.z()*cos(phi) - vector.x()*sin(phi));
-    double new_dir_z = vector.z() * cos(theta) + sqrt(1 - vector.z()*vector.z()) * sin(theta) * cos(phi);
-    return {new_dir_x, new_dir_y, new_dir_z};
-    // TODO: sample cos(phi) and sin(phi) directly instead. This will be more efficient.
 }
 
 Particle::Particle(Eigen::Vector3d& position, Eigen::Vector3d& direction, double energy) : position_(std::move(position)), direction_(std::move(direction)), energy_(energy) {}
