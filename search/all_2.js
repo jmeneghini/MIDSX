e78f2f886baa7eba75653f1e45c6bf079a4f5a45
jmeneghini <jmeneghini@users.noreply.github.com>
2024-01-20 20:33:25 +0000

    deploy: 1a3a1f82ffe2608acb36c40b3c2e3cdf397fea71

--- search/all_2.js
+++ search/all_2.js
@@ -1,13 +1,14 @@
 var searchData=
 [
   ['carlo_20interactions_20and_20dosage_20simulation_20of_20x_20rays_0',['MIDSX (Monte Carlo Interactions and Dosage Simulation of X-rays)',['../index.html',1,'']]],
-  ['cmake_1',['Installation (CMake)',['../index.html#autotoc_md5',1,'']]],
-  ['coherentscattering_2',['CoherentScattering',['../class_coherent_scattering.html',1,'']]],
-  ['computationaldomain_3',['ComputationalDomain',['../class_computational_domain.html',1,'ComputationalDomain'],['../class_computational_domain.html#a2fd89d023d879f74fb28a5bf0d918e00',1,'ComputationalDomain::ComputationalDomain()']]],
-  ['constants_2eh_4',['constants.h',['../constants_8h.html',1,'']]],
-  ['contents_5',['Table of Contents',['../index.html#autotoc_md1',1,'']]],
-  ['continuousinversion_6',['ContinuousInversion',['../class_probability_dist_1_1_continuous_inversion.html',1,'ProbabilityDist::ContinuousInversion'],['../class_probability_dist_1_1_continuous_inversion.html#aadbb8a5fccf0d2cd75b03d11605519fd',1,'ProbabilityDist::ContinuousInversion::ContinuousInversion()']]],
-  ['countsurfacequantity_7',['CountSurfaceQuantity',['../class_count_surface_quantity.html',1,'CountSurfaceQuantity'],['../class_count_surface_quantity.html#a87b54ab7d567b72daed85249be210b15',1,'CountSurfaceQuantity::CountSurfaceQuantity()']]],
-  ['countvalue_8',['CountValue',['../class_count_value.html',1,'']]],
-  ['countvolumequantity_9',['CountVolumeQuantity',['../class_count_volume_quantity.html',1,'CountVolumeQuantity'],['../class_count_volume_quantity.html#a6802a5240cc74d15dc0f3005a95bef04',1,'CountVolumeQuantity::CountVolumeQuantity()']]]
+  ['clear_1',['clear',['../class_surface_quantity_container.html#a702ac55f9433911eb74895503d1886b5',1,'SurfaceQuantityContainer::clear()'],['../class_volume_quantity_container.html#afb81888e3309193eb2ea85b09703f03a',1,'VolumeQuantityContainer::clear()']]],
+  ['cmake_2',['Installation (CMake)',['../index.html#autotoc_md5',1,'']]],
+  ['coherentscattering_3',['CoherentScattering',['../class_coherent_scattering.html',1,'']]],
+  ['computationaldomain_4',['ComputationalDomain',['../class_computational_domain.html',1,'ComputationalDomain'],['../class_computational_domain.html#a2fd89d023d879f74fb28a5bf0d918e00',1,'ComputationalDomain::ComputationalDomain()']]],
+  ['constants_2eh_5',['constants.h',['../constants_8h.html',1,'']]],
+  ['contents_6',['Table of Contents',['../index.html#autotoc_md1',1,'']]],
+  ['continuousinversion_7',['ContinuousInversion',['../class_probability_dist_1_1_continuous_inversion.html',1,'ProbabilityDist::ContinuousInversion'],['../class_probability_dist_1_1_continuous_inversion.html#aadbb8a5fccf0d2cd75b03d11605519fd',1,'ProbabilityDist::ContinuousInversion::ContinuousInversion()']]],
+  ['countsurfacequantity_8',['CountSurfaceQuantity',['../class_count_surface_quantity.html',1,'CountSurfaceQuantity'],['../class_count_surface_quantity.html#a87b54ab7d567b72daed85249be210b15',1,'CountSurfaceQuantity::CountSurfaceQuantity()']]],
+  ['countvalue_9',['CountValue',['../class_count_value.html',1,'']]],
+  ['countvolumequantity_10',['CountVolumeQuantity',['../class_count_volume_quantity.html',1,'CountVolumeQuantity'],['../class_count_volume_quantity.html#a6802a5240cc74d15dc0f3005a95bef04',1,'CountVolumeQuantity::CountVolumeQuantity()']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['computationaldomain_0',['ComputationalDomain',['../class_computational_domain.html#a2fd89d023d879f74fb28a5bf0d918e00',1,'ComputationalDomain']]],
-  ['continuousinversion_1',['ContinuousInversion',['../class_probability_dist_1_1_continuous_inversion.html#aadbb8a5fccf0d2cd75b03d11605519fd',1,'ProbabilityDist::ContinuousInversion']]],
-  ['countsurfacequantity_2',['CountSurfaceQuantity',['../class_count_surface_quantity.html#a87b54ab7d567b72daed85249be210b15',1,'CountSurfaceQuantity']]],
-  ['countvolumequantity_3',['CountVolumeQuantity',['../class_count_volume_quantity.html#a6802a5240cc74d15dc0f3005a95bef04',1,'CountVolumeQuantity']]]
+  ['clear_0',['clear',['../class_surface_quantity_container.html#a702ac55f9433911eb74895503d1886b5',1,'SurfaceQuantityContainer::clear()'],['../class_volume_quantity_container.html#afb81888e3309193eb2ea85b09703f03a',1,'VolumeQuantityContainer::clear()']]],
+  ['computationaldomain_1',['ComputationalDomain',['../class_computational_domain.html#a2fd89d023d879f74fb28a5bf0d918e00',1,'ComputationalDomain']]],
+  ['continuousinversion_2',['ContinuousInversion',['../class_probability_dist_1_1_continuous_inversion.html#aadbb8a5fccf0d2cd75b03d11605519fd',1,'ProbabilityDist::ContinuousInversion']]],
+  ['countsurfacequantity_3',['CountSurfaceQuantity',['../class_count_surface_quantity.html#a87b54ab7d567b72daed85249be210b15',1,'CountSurfaceQuantity']]],
+  ['countvolumequantity_4',['CountVolumeQuantity',['../class_count_volume_quantity.html#a6802a5240cc74d15dc0f3005a95bef04',1,'CountVolumeQuantity']]]
 ];
